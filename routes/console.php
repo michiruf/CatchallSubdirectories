e8055917c5686c828a708ce29136102855be7438
Michael Ruf <git@michiruf.de>
2024-05-24 19:53:49 +0200

    Add horizon snapshot command for metrics in the horizon dashboard

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Schedule;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 use Spatie\Health\Commands\ScheduleCheckHeartbeatCommand;
 
+// App
 Schedule::command(CatchAllSubdirectoriesCommand::class)->withoutOverlapping()->everyFiveMinutes();
 
+// Horizon
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
+
+// Health
 Schedule::command(ScheduleCheckHeartbeatCommand::class)->everyMinute();
 Schedule::command(RunHealthChecksCommand::class)->everyMinute();
