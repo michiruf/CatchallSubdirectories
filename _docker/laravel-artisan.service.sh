40a68cd3c1412838460dbd9e643d083d79cda4fc
Michael Ruf <git@michiruf.de>
2024-06-02 22:11:15 +0200

    Add baked variant and adjust deployer variant

--- _docker/laravel-artisan.service.sh
+++ _docker/laravel-artisan.service.sh
@@ -2,20 +2,18 @@
 # shellcheck shell=sh
 set -e
 
-dir="/app/current"
-
-if [ ! -d "$dir" ]; then
-    echo "cannot execute artisan command \"$*\" yet: deployment not yet complete (there is no $dir directory)"
+if [ ! -d "$LARAVEL_APPLICATION_PATH" ]; then
+    echo "cannot execute artisan command \"$*\" yet: deployment not yet complete (there is no $LARAVEL_APPLICATION_PATH directory)"
     exit 1
 fi
 
-if [ ! -f "$dir/artisan" ]; then
-    echo "cannot execute artisan command \"$*\" yet: deployment not yet complete (there is no artisan file)"
+if [ ! -f "$LARAVEL_APPLICATION_PATH/artisan" ]; then
+    echo "cannot execute artisan command \"$*\" yet: deployment not yet complete (there is no artisan file in $LARAVEL_APPLICATION_PATH)"
     exit 1
 fi
 
 # Some commands require to be in the artisan directory already, so we first need to switch directories
-cd "$dir"
+cd "$LARAVEL_APPLICATION_PATH"
 php artisan "$@"
 
 # Exit with artisans exit code
