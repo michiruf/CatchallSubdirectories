bdeb72c5f44e6030b0fc4e7a82282aea0b7c7d4b
Michael Ruf <git@michiruf.de>
2024-08-17 17:08:38 +0200

    Fix asset resolution behind a reverse proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Imap\LazyInitializedConnection;
 use Ddeboer\Imap\ConnectionInterface;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        // Force usage of https when loading vite resources on a production or staging server
+        $isProduction = config('app.env') === 'production';
+        $isStaging = config('app.env') === 'staging' || str(config('app.url'))->contains('staging');
+        if ($isProduction || $isStaging) {
+            URL::forceScheme('https');
+
+            // Set that the request was made via https, because else signatures cannot get validated
+            request()->server->set('HTTPS', request()->header('X-Forwarded-Proto', 'https') == 'https' ? 'on' : 'off');
+        }
     }
 }
